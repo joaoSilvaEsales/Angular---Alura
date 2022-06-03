e5e52671e6871db32014dc73dd603fffac9883ca
João Carvalho <joao.silva@esales.com.br>
2022-06-03 17:39:34 -0300

    Adicionando função de transferencia

--- src/app/nova transferencia/nova-transferencia.component.ts
+++ src/app/nova transferencia/nova-transferencia.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 @Component({
-  selector:'app-nova-transferencia',
-  templateUrl:'./nova-transferencia.component.html',
-  styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+  valor: number = 0;
+  destino: number = 1000;
 
+  transferir() {
+    console.log('Solicitada Nova Transferencia');
+    console.log('Valor: ', this.valor)
+    console.log('Destino: ', this.destino)
+  }
 }
